d4c7e82dc72abc4272de367d2940f1884ee9f840
bala-amypo <bala@amypo.in>
2025-12-18 05:08:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -0,0 +1,86 @@
+package com.example.demo.entity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.Instant;
+
+public class Contract {
+    Long id;
+    String contractNumber;
+    String title;
+    String counterpartyName;
+    Date agreedDeliveryDate;
+    BigDecimal baseContractValue;
+    String status;
+    Instant createdAt;
+    Instant updatedAt;
+
+    public Contract(Long id, String contractNumber, String title, String counterpartyName, Date agreedDeliveryDate,
+            BigDecimal baseContractValue, String status, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.contractNumber = contractNumber;
+        this.title = title;
+        this.counterpartyName = counterpartyName;
+        this.agreedDeliveryDate = agreedDeliveryDate;
+        this.baseContractValue = baseContractValue;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getContractNumber() {
+        return contractNumber;
+    }
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getCounterpartyName() {
+        return counterpartyName;
+    }
+    public void setCounterpartyName(String counterpartyName) {
+        this.counterpartyName = counterpartyName;
+    }
+    public Date getAgreedDeliveryDate() {
+        return agreedDeliveryDate;
+    }
+    public void setAgreedDeliveryDate(Date agreedDeliveryDate) {
+        this.agreedDeliveryDate = agreedDeliveryDate;
+    }
+    public BigDecimal getBaseContractValue() {
+        return baseContractValue;
+    }
+    public void setBaseContractValue(BigDecimal baseContractValue) {
+        this.baseContractValue = baseContractValue;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
