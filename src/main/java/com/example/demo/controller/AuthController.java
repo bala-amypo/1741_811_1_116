208d7bf515e4e31c26765962def2c4b727202177
bala-amypo <bala@amypo.in>
2025-12-19 09:55:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,8 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 @RestController
 public class AuthController {
 
+    @Autowired
+    public UserService serv;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody Map<String , String> req ){
+        String email = req.get("email");
+        String password = req.get("password");
+        return serv.registerUser(email, password);
+    }
+
+    @PostMapping("/login")
+    public Optional<User> loginUser(@RequestBody String email ){
+        return serv.getUserByEmail(email);
+    }
 }
