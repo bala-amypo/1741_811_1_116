b3e48d881f3685c3a04d449ec45b0f2f270bf8a3
bala-amypo <bala@amypo.in>
2025-12-26 13:29:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/DeliveryRecordDto.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+public class DeliveryRecordDto {
+    private String notes;
+    public String getNotes() { return notes; }
+    public void setNotes(String notes) { this.notes = notes; }
+}
